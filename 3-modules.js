84cd8a5b9312ddb9497f904a4c4cd2101a8824d8
AgusR-99 <agus.socialclub@gmail.com>
2022-05-09 20:31:25 -0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// CommonmJS, every file is module (by default)
+//Modules - Encapsulated Code (only share minimum)
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-favlor");
+require("./7-mind-grenade");
+sayHi(names.john);
+sayHi("susan");
+sayHi(names.peter);
